5d1cf13ad39dfe175ef23babe7609fe20bd91d56
Luna9804 <56940000+Luna9804@users.noreply.github.com>
2021-04-12 17:39:06 -0400

    cresting Room file

---
+++ src/Room.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import React from "react";
+
+export default function Room() {
+  return (
+    <div className ="Room">
+      <h1>R1</h1>
+      <h2> </h2>
+    </div>
+  );
+}
