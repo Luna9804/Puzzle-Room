01dc3fcf4c08eeaeb5fc5b09dba9407203cf8a13
Luna9804 <56940000+Luna9804@users.noreply.github.com>
2021-04-19 17:45:13 -0400

    Worked on click handler

--- src/Room.js
+++ src/Room.js
@@ -1,11 +1,18 @@
 import "./styles.css";
 import React from "react";
 
-export default function Room() {
-  return (
-    <div className ="Room">
-      <h1>R1</h1>
-      <h2> </h2>
-    </div>
-  );
+export default class Room extends React.Component {
+handleClick(){
+console.log("Test");
+}
+  render() {
+    return (
+      <div className="Room">
+        <h1>R1</h1>
+
+        <button onClick = {this.handleClick()}> button</button>
+        <h2> </h2>
+      </div>
+    );
+  }
 }
