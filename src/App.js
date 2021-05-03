73c60c6f1a48fbf2acf229e1b55b50b2c2cc4dec
ImaniT16 <43816835+ImaniT16@users.noreply.github.com>
2021-05-03 17:50:35 -0400

    Room385

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./styles.css";
 import Room from "./Room.js";
+import React, { useState } from "react";
 
 export default function App() {
+  let displeyroom = <Room />;
+  let [isButtonPressed, setButtonPressed] = useState(false);
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <Room />
     </div>
-    
   );
 }
