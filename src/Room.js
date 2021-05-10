5271583e373bb18467baaf1188dbd56c89a691f4
Luna9804 <56940000+Luna9804@users.noreply.github.com>
2021-05-10 17:49:03 -0400

    Worked on Room one and two working on room 2

--- src/Room.js
+++ src/Room.js
@@ -1,11 +1,17 @@
 import "./styles.css";
 import React, { useState } from "react";
+import Room2 from "./Room2.js";
 
 export default function Room() {
   // TODO: Create state "isButtonPressed".
   // let [ isAnswered, setIsAnswered ] = useState(false);
   let [isButtonPressed, setButtonPressed] = useState(false);
-
+  let Room1;
+  if (isButtonPressed === false){
+    Room1 = <div><h1>R1</h1><button onClick={() => handleClick()}>button: {isButtonPressed}</button></div>
+  }else{
+    Room1 = <Room2/>;
+  }
   let handleClick = () => {
     console.log("Test");
     setButtonPressed(true);
@@ -13,10 +19,7 @@ export default function Room() {
 
   return (
     <div className="Room">
-      <h1>R1</h1>
-      {isButtonPressed.toString()}
-      <button onClick={() => handleClick()}>button: {isButtonPressed}</button>
-
+      {Room1}
       <h2> </h2>
     </div>
   );
---
+++ src/Room3.js
@@ -0,0 +1,5 @@
+import "./styles.css";
+import React, { useState } from "react";
+export default function Room2() {
+  return <div className="Room3">Room3 </div>
+}
